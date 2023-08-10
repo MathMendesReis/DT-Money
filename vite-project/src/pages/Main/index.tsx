e9a518b81b207912dd34234043bbb6527b523fc4
MathMendesReis <matheusmendesreis@outlook.com>
2023-08-10 14:58:02 -0300

    estrura html pronta

--- vite-project/src/pages/Main/index.tsx
+++ vite-project/src/pages/Main/index.tsx
@@ -1,7 +1,17 @@
+import Logo from '../../assets/react.svg'
+
 export default function Main() {
   return (
     <div>
-
+      <header>
+        <div>
+          <img src={Logo} alt="" />
+          <h1>DT Money</h1>
+        </div>
+        <button>
+          <span>Nova Transação</span>
+        </button>
+      </header>
     </div>
   )
 }
