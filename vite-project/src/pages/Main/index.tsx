9da7c19bbe43b72be349c45e03975bf3f7229dc7
MathMendesReis <matheusmendesreis@outlook.com>
2023-08-11 10:47:00 -0300

    estrutura css e html pronta

--- vite-project/src/pages/Main/index.tsx
+++ vite-project/src/pages/Main/index.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import Logo from '../../assets/react.svg'
 import Sumary from '../components/sumary'
-import { ContainerHeader, ContainerInput } from './styled'
+import { ButtonModel, ButtonModelCadastrar, ButtonModelClose, ContainerHeader, ContainerInput, ContainerModal, ContainerTable, ContainerTableDiv, InputStyled } from './styled'
 
 export default function Main() {
+  const [showDisplaModal,setshowDisplaModal] = useState<boolean>(false)
   return (
     <div>
       <ContainerHeader>
@@ -10,7 +12,11 @@ export default function Main() {
           <img src={Logo} alt="" />
           <h1>DT Money</h1>
         </div>
-        <button>
+        <button
+        onClick={()=>{
+          setshowDisplaModal(true)
+        }}
+        >
           <span>Nova Transação</span>
         </button>
       </ContainerHeader>
@@ -21,6 +27,60 @@ export default function Main() {
         type="text" />
         <button>buscar</button>
       </ContainerInput>
+      <ContainerTableDiv>
+        <ContainerTable>
+        <thead>
+            <tr>
+              <th>desenvolvimento do site</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>12.000</td>
+              <td>venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </ContainerTable>
+      </ContainerTableDiv>
+      <div>
+      {showDisplaModal && (
+        <ContainerModal>
+          <div>
+            <ButtonModelClose
+            onClick={()=>{
+              setshowDisplaModal(false)
+            }}
+            >
+              x
+            </ButtonModelClose>
+
+            <form action="">
+              <span>Nova transação</span>
+              <InputStyled type="text"
+              placeholder='Descriçao'
+              />
+              <InputStyled type="text"
+              placeholder='Preço'
+              />
+
+              <InputStyled type="text"
+              placeholder='Categoria'
+              />
+
+              <label htmlFor="">
+                <ButtonModel>entrada</ButtonModel>
+                <ButtonModel>saida</ButtonModel>
+              </label>
+
+              <ButtonModelCadastrar
+              type='submit'
+              >Cadastrar</ButtonModelCadastrar>
+            </form>
+          </div>
+        </ContainerModal>
+      )}
+      </div>
     </div>
   )
 }
